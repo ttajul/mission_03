a1140d12d7665f3ddce84e82e28bff963fbda362
Tajul Mazlan <tajulm@missionreadyhq.com>
2024-07-14 12:43:31 +1200

    create controller and route for AI backend

--- mission_03/backend/src/app.js
+++ mission_03/backend/src/app.js
@@ -15,16 +15,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-// routes
+//routes
+const aiRoutes = require('./routes/aiRoutes');
+
+//endpoints
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-//AI initialization
-const { GoogleGenerativeAI } = require("@google/generative-ai");
-// Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+app.use('/api/ai', aiRoutes); 
 
 // Database connection
 if (process.env.NODE_ENV !== "test") {
@@ -34,20 +33,14 @@ if (process.env.NODE_ENV !== "test") {
   });
 }
 
+
+
 const PORT = 3000; // Ensure this is set to 3000
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
 
-async function run() {
-  const prompt = "Write a story about a AI and magic"
 
-  const result = await model.generateContent(prompt);
-  const response = await result.response;
-  const text = response.text();
-  console.log(text);
-}
 
-run();
 module.exports = app;
 
